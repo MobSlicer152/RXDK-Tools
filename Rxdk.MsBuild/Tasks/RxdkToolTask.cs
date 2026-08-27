f2abc3e7c9a159b9870db42cade52069b12af912
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-27 01:29:17 -0400

    msbuild: Start implementing MSBuild integration

---
+++ Rxdk.MsBuild/Tasks/RxdkToolTask.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.CPPTasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rxdk.MsBuild.Tasks
+{
+    public abstract class RxdkToolTask : TrackedVCToolTask
+    {
+        protected RxdkToolTask()
+            : base(new ResourceManager("Microsoft.Build.CPPTasks.Strings", Assembly.GetAssembly(typeof(TrackedVCToolTask))))
+        {
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            return ToolName;
+        }
+    }
+}
---
+++ Rxdk.MsBuild/Tasks/ZigCompile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rxdk.MsBuild.Tasks
+{
+    public class ZigCompile : RxdkToolTask
+    {
+        protected override string TrackerIntermediateDirectory => throw new NotImplementedException();
+        protected override ITaskItem[] TrackedInputFiles => throw new NotImplementedException();
+        protected override ArrayList SwitchOrderList => switchOrderList;
+        protected ArrayList switchOrderList;
+
+        public ZigCompile()
+        {
+            switchOrderList = new ArrayList()
+            {
+                "AlwaysAppend"
+            };
+        }
+
+        protected override string ToolName => "zig.exe";
+        protected override string AlwaysAppend => "cc";
+    }
+}
